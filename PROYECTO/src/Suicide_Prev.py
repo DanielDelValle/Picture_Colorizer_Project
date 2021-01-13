1368a0075ec330107ed596ba0c7ff5adcfd07cb1
DaniValle <guindasman@gmail.com>
2021-01-13 17:31:57 +0100

    Folder Refreshed

--- PROYECTO/src/Suicide_Prev.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import numpy as np
-
-suicide = pd.read_csv("documentation\\who_suicide_statistics.csv")
-suicide = suicide.rename(columns={'sex': 'gender'})
-suicide['age'] = suicide['age'].str.replace('years', '')
-
-suicide.set_index('country', inplace=True)
-suic = suicide
