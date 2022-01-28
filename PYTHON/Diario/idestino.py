a5228b2b12a7cdd43f85e3c9b51e103a3015f693
DaniValle <danimolar@hotmail.com>
2022-01-28 14:10:20 +0100

    folders reorganised and cleaned

--- PYTHON/Diario/idestino.py
+++
@@ -1,4 +0,0 @@
-import iorigen
-print(iorigen.tt)
-import iorigen
-print(iorigen.f5(3))
--- PYTHON/Diario/iorigen.py
+++
@@ -1,4 +0,0 @@
-tt = "importación exitosa"
-def f5(a):
-    for x in range (a):
-        return "el valor del cuadrado es: " , a**2
